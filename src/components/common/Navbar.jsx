cae51d0503211877aeb79439f2b7bef427cbf9c1
Suryansh Bhatnagar <sbh2068@gmail.com>
2024-04-21 23:19:33 +0530

    some ui fixes in navbar

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { AiOutlineMenu, AiOutlineShoppingCart } from "react-icons/ai"
+import { AiOutlineClose, AiOutlineMenu, AiOutlineShoppingCart } from "react-icons/ai"
 import { BsChevronDown } from "react-icons/bs"
 import { useSelector } from "react-redux"
 import { Link, matchPath, useLocation } from "react-router-dom"
@@ -19,6 +19,7 @@ function Navbar() {
 
   const [subLinks, setSubLinks] = useState([])
   const [loading, setLoading] = useState(false)
+  const [showMenu,setShowMenu] = useState(false);
 
   useEffect(() => {
     ;(async () => {
@@ -48,8 +49,56 @@ function Navbar() {
       <div className="flex w-11/12 max-w-maxContent items-center justify-between">
         {/* Logo */}
         <Link to="/">
-          <img src={logo} alt="Logo" width={160} height={32} loading="lazy" />
+          <img src={logo} alt="Logo" className="w-32 md:w-40 "  loading="lazy" />
         </Link>
+        <div
+            className={`group relative flex md:hidden cursor-pointer items-center gap-1 ${
+                        matchRoute("/catalog/:catalogName")
+                          ? "text-yellow-25"
+                          : "text-richblack-25"
+                      }`}
+                    >
+                      <p>Catalog</p>
+                      <BsChevronDown />
+                      <div className="invisible absolute left-[50%] top-[50%] z-[1000] flex w-[200px] translate-x-[-50%] translate-y-[3em] flex-col rounded-lg bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all duration-150 group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100 lg:w-[300px]">
+                        <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5"></div>
+                        {loading ? (
+                          <p className="text-center">Loading...</p>
+                        ) : subLinks?.length ? (
+                          <>
+                            {subLinks
+                              ?.map((subLink, i) => (
+                                <Link
+                                  to={`/catalog/${subLink.name
+                                    .split(" ")
+                                    .join("-")
+                                    .toLowerCase()}`}
+                                  className="rounded-lg bg-transparent text-richblack-700 py-4 pl-4 hover:bg-richblack-50"
+                                  key={i}
+                                >
+                                  <p>{subLink.name}</p>
+                                </Link>
+                              ))}
+                          </>
+                        ) : (
+                          <p className="text-center">No Courses Found</p>
+                        )}
+                      </div>
+                    </div>
+       {showMenu && <div class="absolute md:hidden top-[6%] right-1 p-2 z-[1000] divide-y-[1px] divide-richblack-700 w-[120px] overflow-hidden rounded-md border-[1px] border-richblack-700 bg-richblack-800 ">
+        <li class="list-none"><Link to="/" onClick={()=>setShowMenu(false)}><p className={`${ matchRoute("/")
+                          ? "text-yellow-25"
+                          : "text-richblack-25"} py-2`}>Home</p></Link></li>
+        <li class="list-none"><Link to="/about" onClick={()=>setShowMenu(false)} ><p className={`${ matchRoute("/about")
+                          ? "text-yellow-25"
+                          : "text-richblack-25"} py-2`} >About Us</p></Link></li>
+        <li class="list-none"><Link to="/login" onClick={()=>setShowMenu(false)}><p className={`${ matchRoute("/login")
+                          ? "text-yellow-25"
+                          : "text-richblack-25"} py-2`} >Log in</p></Link></li>
+        <li class="list-none"><Link to="/signup" onClick={()=>setShowMenu(false)}><p className={`${ matchRoute("/signup")
+                          ? "text-yellow-25"
+                          : "text-richblack-25"} py-2`} >Sign up</p></Link></li>
+        </div>}
         {/* Navigation links */}
         <nav className="hidden md:block">
           <ul className="flex gap-x-6 text-richblack-25">
@@ -140,9 +189,13 @@ function Navbar() {
           )}
           {token !== null && <ProfileDropdown />}
         </div>
-        <button className="mr-4 md:hidden">
-          <AiOutlineMenu fontSize={24} fill="#AFB2BF" />
+        <button className="mr-4 md:hidden" onClick={()=>setShowMenu(!showMenu)}>
+        {
+
+        showMenu ?  <AiOutlineClose fontSize={24} fill="#AFB2BF" /> : <AiOutlineMenu fontSize={24} fill="#AFB2BF" /> 
+        } 
         </button>
+       
       </div>
     </div>
   )
