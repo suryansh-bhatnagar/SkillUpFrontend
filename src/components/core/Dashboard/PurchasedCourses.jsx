f5daf0ea7e7a4f5b53abf14d946a75fb93decbf0
Suryansh Bhatnagar <sbh2068@gmail.com>
2025-06-02 21:41:16 +0530

    ui change

---
+++ src/components/core/Dashboard/PurchasedCourses.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react"
+import ProgressBar from "@ramonak/react-progress-bar"
+import { useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
+
+import { getUserEnrolledCourses } from "../../../services/operations/profileAPI"
+
+export default function PurchasedCourses() {
+  const { token } = useSelector((state) => state.auth)
+  const navigate = useNavigate()
+
+  const [enrolledCourses, setEnrolledCourses] = useState(null)
+  // const getEnrolledCourses = async () => {
+  //   try {
+  //     const res = await getUserEnrolledCourses(token);
+  //     setEnrolledCourses(res);
+  //   } catch (error) {
+  //     console.log("Could not fetch enrolled courses.")
+  //   }
+  // };
+  useEffect(() => {
+    const getEnrolledCourses = async () => {
+      try {
+        const res = await getUserEnrolledCourses(token);
+        setEnrolledCourses(res);
+      } catch (error) {
+        console.log("Could not fetch enrolled courses.")
+      }
+    };
+    getEnrolledCourses();
+  }, [token])
+
+  return (
+    <>
+      <div className="text-3xl text-richblack-50">Purchased Courses</div>
+        <p className="grid h-[10vh] w-full place-content-center text-richblack-5">
+          You have not Purchased any course yet.
+        </p>
+    </>
+  )
+}
