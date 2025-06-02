5a6758739466626a58d7922db35cd290cb2e88d8
Suryansh Bhatnagar <sbh2068@gmail.com>
2025-06-02 21:47:07 +0530

    minor fix for deployment

--- src/components/core/Dashboard/PurchasedCourses.jsx
+++ src/components/core/Dashboard/PurchasedCourses.jsx
@@ -1,30 +1,4 @@
-import { useEffect, useState } from "react"
-import { useSelector } from "react-redux"
-
-
-import { getUserEnrolledCourses } from "../../../services/operations/profileAPI"
-
 export default function PurchasedCourses() {
-  const { token } = useSelector((state) => state.auth)
-  // const getEnrolledCourses = async () => {
-  //   try {
-  //     const res = await getUserEnrolledCourses(token);
-  //     setEnrolledCourses(res);
-  //   } catch (error) {
-  //     console.log("Could not fetch enrolled courses.")
-  //   }
-  // };
-  useEffect(() => {
-    const getEnrolledCourses = async () => {
-      try {
-        const res = await getUserEnrolledCourses(token);
-        setEnrolledCourses(res);
-      } catch (error) {
-        console.log("Could not fetch enrolled courses.")
-      }
-    };
-    getEnrolledCourses();
-  }, [token])
 
   return (
     <>
